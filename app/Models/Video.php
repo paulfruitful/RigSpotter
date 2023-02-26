b4e942ae3294a55815ec2da896d592d0f30f1c35
Paul Fruitful <fruitful2007@outlook.com>
2023-02-26 15:50:48 +0100

    Fillables

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     use HasFactory;
+    protected $fillables=[
+        'video',
+        'title',
+        'description',
+        'tags',
+        'location',
+        'email'
+    ];
+
 }
