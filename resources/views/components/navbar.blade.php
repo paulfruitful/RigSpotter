6dd61938ceddfd4aeb784790b8e3797fcae3469c
Paul Fruitful <fruitful2007@outlook.com>
2023-03-01 00:02:19 +0100

    View

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,6 @@
+<div class="grid justify-self-stretch p-8 w-full">
+    <div class="justify-self-start">
+       <a href="/" class="font-bold text-green-800 text-3xl">RigSpotter</a>
+    </div>
+    
+</div>
