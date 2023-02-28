d7b905c1469afd54f6e854768102d6e3e32c3c44
Paul Fruitful <fruitful2007@outlook.com>
2023-02-28 23:33:22 +0100

    All videos function

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class VideoController extends Controller
 {
     //
     public function all(){
+        $videos=Video::latest();
+        return view('videos',compact('videos'));
 
     }
 }
