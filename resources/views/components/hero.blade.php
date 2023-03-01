a6fc76bf9cf616232ce6e524be0c07ccfb01979a
Paul Fruitful <fruitful2007@outlook.com>
2023-03-01 02:20:39 +0100

    Hero

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,3 +1,3 @@
-<div class="grid justify-items-center  bg-white bg-fixed  p-8 w-full" style="height:500px; background-image: url(https://images.theconversation.com/files/365193/original/file-20201023-19-1bfhjtd.jpg?ixlib=rb-1.1.0&rect=19%2C31%2C2553%2C1652&q=45&auto=format&w=926&fit=clip)">
+<div class="grid justify-items-center  bg-white bg-fixed  p-8 w-full" style="height:500px; background-image: url(https://th.bing.com/th/id/OIF.y5Ziid1LYhxrdh0iYhHU7g?pid=ImgDet&rs=1)">
 
 </div>
